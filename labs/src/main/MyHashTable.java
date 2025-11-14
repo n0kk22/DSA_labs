e98d2851e3ba29d6b2ffada1d86a7de1fb1cbb35
n0kk22 <nomiko94222gmail.com>
2025-11-14 20:13:54 +0800

    Commit

---
+++ labs/src/main/MyHashTable.java
@@ -0,0 +1,24 @@
+package main;
+
+import dataStructures.HashTable;
+
+public class MyHashTable extends HashTable{
+
+    public MyHashTable(int theDivisor) {
+        super(theDivisor);
+    }
+
+    public Object updateElement(Object theKey, Object theElement){
+
+        return elementToUpdate;
+    }
+
+    public Object updateKey(Object theKey, Object theNewKey){
+        return elementToUpdate;
+    }
+
+    public void delete(Object theKey){
+
+    }
+
+}
